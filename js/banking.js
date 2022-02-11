3173d96e79e9929f460b95bae171e335211b4c93
Marzan-Bin-Rahman <mail.to.marzan@gmail.com>
2022-02-11 21:52:58 +0600

    update done

--- js/banking.js
+++ js/banking.js
@@ -1,23 +1,28 @@
-function getInputValue(){
-    const depositInput = document.getElementById('deposit-input');
+function getInputValue(inputId){
+    const depositInput = document.getElementById(inputId);
     const depositInputText = depositInput.value;
     const depositAmount = parseFloat(depositInputText);
     depositInput.value = "";
     return depositAmount;
 }
 
-function addingToTotal(depositAmount){
-    const depositTotal = document.getElementById('deposit-total');
+function addingToTotal(inputId, depositAmount){
+    const depositTotal = document.getElementById(inputId);
     const depositTotalText = depositTotal.innerText;
     const depositTotalAmount = parseFloat(depositTotalText);
     depositTotal.innerText = depositTotalAmount + depositAmount;
 }
 
-function updateTotal(){
+function updateTotal(isAdd){
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
     const balanceTotalAmount = parseFloat(balanceTotalText);
-    balanceTotal.innerText = balanceTotalAmount + depositAmount;
+    if(isAdd == true){
+        balanceTotal.innerText = balanceTotalAmount + depositAmount;
+    }
+    else{
+        balanceTotal.innerText = balanceTotalAmount - depositAmount;
+    }
 }
 
 // game with deposit
@@ -26,13 +31,17 @@ document.getElementById('deposit-button').addEventListener('click', function(){
 /*     const depositInput = document.getElementById('deposit-input');
     const depositInputText = depositInput.value;
     const depositAmount = parseFloat(depositInputText); */
-    depositAmount = getInputValue();
+    depositAmount = getInputValue('deposit-input');
+    addingToTotal("deposit-total", depositAmount);
+    updateTotal(true);
+
+
     // getting the amount
     /* const depositTotal = document.getElementById('deposit-total');
     const depositTotalText = depositTotal.innerText;
     const depositTotalAmount = parseFloat(depositTotalText);
     depositTotal.innerText = depositTotalAmount + depositAmount; */
-    addingToTotal(depositAmount);
+    
     // clearing the input
     // depositInput.value = "";
 
@@ -41,8 +50,34 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     const balanceTotalText = balanceTotal.innerText;
     const balanceTotalAmount = parseFloat(balanceTotalText);
     balanceTotal.innerText = balanceTotalAmount + depositAmount; */
-    updateTotal();
+    
 })
 
 // game with deposit
 
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    withdrawAmount = getInputValue('withdraw-input');
+    addingToTotal("withdraw-total", withdrawAmount);
+    updateTotal(false);
+    
+    /* const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawInputText = withdrawInput.value;
+    const withdrawAmount = parseFloat(withdrawInputText); */
+    
+    
+
+    /* const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalText = withdrawTotal.innerText;
+    const withdrawTotalAmount = parseFloat(withdrawTotalText);
+    withdrawTotal.innerText = withdrawTotalAmount + withdrawAmount; */
+
+    
+    // withdrawInput.value = "";
+
+/*     const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const balanceTotalAmount = parseFloat(balanceTotalText);
+    balanceTotal.innerText = balanceTotalAmount - withdrawAmount; */
+
+    
+})
